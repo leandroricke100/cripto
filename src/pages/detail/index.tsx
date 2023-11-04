1885e3f4836e4449c822d3d0e41371fba89f937c
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-11-04 12:40:40 -0300

    inserindo rotas

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,7 @@
+export function Detail() {
+  return (
+    <div>
+      <h1>Pagina detalhes</h1>
+    </div>
+  );
+}
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,7 @@
+export function Notfound() {
+  return (
+    <div>
+      <h1>Pagina 404 nao existe</h1>
+    </div>
+  );
+}
